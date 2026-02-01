15e48bc2241db6034ee6367a63b213bafee928f5
RotColton <maromidomingo@gmail.com>
2026-02-01 21:09:53 +0100

    doc: add initial README

--- src/main/kotlin/infrastructure/driver/rest/routes/player/request/AddCreatureRequest.kt
+++ src/main/kotlin/infrastructure/driver/rest/routes/player/request/AddCreatureRequest.kt
@@ -2,7 +2,7 @@ package com.romina.infrastructure.driven.rest.routes.player.request
 
 import com.romina.application.domain.model.CreatureClass
 import kotlinx.serialization.Serializable
-
+//TODO: validation rules
 @Serializable
 data class AddCreatureRequest(
     val creatureName: String,
--- src/main/kotlin/infrastructure/driver/rest/routes/player/request/CreatePlayerRequest.kt
+++ src/main/kotlin/infrastructure/driver/rest/routes/player/request/CreatePlayerRequest.kt
@@ -1,7 +1,7 @@
 package com.romina.infrastructure.driven.rest.routes.player.request
 
 import kotlinx.serialization.Serializable
-
+//TODO: validation rules
 @Serializable
 data class CreatePlayerRequest(
     val playerName : String
